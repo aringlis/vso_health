8a7fb3b59cdb7cca1114825125c626a13bcc4d18
Andrew Inglis <andrew.inglis@nasa.gov>
2022-06-15 15:00:34 -0400

    adding the performance vs time plot creation

--- vso_health_script.py
+++ vso_health_script.py
@@ -3,6 +3,8 @@
 from query_vso_health import query_vso_providers, create_master_status_file, cleanup_query_files
 from vso_bokeh_plotting import vso_health_bokeh_plot, convert_html_to_png
 #from vso_report_helpers import generate_report
+from vso_report_helpers import percent_good
+from plot_performance import plot_performance
 
 # instruments in the skip list will not be tested
 skiplist = ['K-Cor','chp','dpm','mk4','cp','ChroTel','BCS','HXT','SXT','WBS','XRT','CFDT1','CFDT2']
@@ -17,10 +19,9 @@ df_master, df = create_master_status_file()
 vso_health_bokeh_plot()
 #convert_html_to_png()
 
-# generate a report of the latest VSO health check
-#report_fname = generate_report(fname)
-
-#from vso_report_helpers import send_report_email_alternative
-#send_report_email_alternative(fname)
+# write out the percent txt files
+percent_good(fname)
 
 cleanup_query_files()
+
+plot_performance()
